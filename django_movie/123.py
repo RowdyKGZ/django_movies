f75734e711ac532d49c44733646f43c2ac3df391
RowdyKGZ <RowdyKG@gmail.com>
2021-03-12 21:20:29 +0600

    aaa

--- django_movie/123.py
+++ django_movie/123.py
@@ -1,6 +1,16 @@
-import json
-str_ = 'abc'
-
-with open('test.json', 'r+') as j:
-    a = json.loads(j.read())
-print(a)
+# [Unit]
+# Description=The NGINX HTTP and reverse proxy server
+# After=syslog.target network.target remote-fs.target nss-lookup.target
+#
+# [Service]
+# Type=forking
+# PIDFile=/run/nginx.pid
+# ExecStartPre=/usr/sbin/nginx -t
+# ExecStart=/usr/sbin/nginx
+# ExecReload=/bin/kill -s HUP $MAINPID
+# ExecStop=/bin/kill -s QUIT $MAINPID
+# PrivateTmp=true
+#
+# [Install]
+# WantedBy=multi-user.target
+#
